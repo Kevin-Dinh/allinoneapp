e45932d871bcf7d33db3fb479a7511b1b4a9551a
Kevin-Dinh <kevin.dinh90@gmail.com>
2016-10-07 13:37:39 +0200

    Add files via upload

--- generic.js
+++ generic.js
@@ -1,10 +1,36 @@
+var information = {
+	greenBackground:"lightgreen",
+	blueBackground:"lightblue",
+	silverBackground:"silver",
+	whiteBackground:"white",
+	brownBackground:"brown",
+	good:"This is good weather and you should visit this city.",
+	bad:"Bad weather, should not go here.",
+	url1:"http://api.openweathermap.org/data/2.5/forecast?",
+	url2:"http://api.population.io/1.0/countries",
+	url3:"http://api.population.io:80/1.0/population/",
+	APPID:"4066916114404bb6ee654b8bbc254890"
+}
+
+$( function() {
+    $( "#datepicker" ).datepicker({
+    	dateFormat: 'yy-mm-dd',
+    });
+  });
+
+function refreshPage(){
+	location.reload();
+}
+
 function search(){
 	var id = document.getElementById("locationId");
 	var locationId = id.value;
-	var urlString = "http://api.openweathermap.org/data/2.5/forecast?id="+ locationId +"&APPID=4066916114404bb6ee654b8bbc254890"
+	var param = {id:locationId, APPID: information.APPID}
+	//@webservice
 	$.ajax({
 		dataType: 'jsonp',
-		url: urlString,
+		url: information.url1,
+		data: param,
 		success: function (response){
 			createDataTable(response);
 		},
@@ -14,7 +40,7 @@ function search(){
 
 function createDataTable(response){
 
-document.getElementById("city").innerHTML = "Weather forecast from " + response.list[0].dt_txt + " to " + response.list[response.list.length -1].dt_txt; 
+	document.getElementById("city").innerHTML = "Weather forecast from " + response.list[0].dt_txt + " to " + response.list[response.list.length -1].dt_txt; 
 	for (var i = 0; i < response.list.length; i++) { 
     	var date = response.list[i].dt_txt;
 		var city = response.city.name;
@@ -24,23 +50,38 @@ document.getElementById("city").innerHTML = "Weather forecast from " + response.
 		var temperature = Math.round((response.list[i].main.temp - 273.15))*100/100;
 
 		var backgroundColor;
-	if(i <= 2){
-		backgroundColor = "lightgreen";
-	}else if (3 <= i && i <= 10) {
-		backgroundColor = "lightblue";
-	} else if(11 <= i && i <= 18) {
-		backgroundColor = "white"
-	} else if(19 <= i && i <= 26) {
-		backgroundColor = "silver"
-	}  else if(27 <= i && i <= 34) {
-		backgroundColor = "brown"
-	} 
+		if(i <= 3){
+			backgroundColor = information.greenBackground;
+		}else if (4 <= i && i <= 11) {
+			backgroundColor = information.blueBackground;
+		} else if(12 <= i && i <= 19) {
+			backgroundColor = information.silverBackground;
+		} else if(20 <= i && i <= 27) {
+			backgroundColor = information.brownBackground;
+		}  else if(28 <= i && i <= 35) {
+			backgroundColor = information.whiteBackground;
+		} 
+		/*backgroundColor = information.greenBackground;
+		if(i >= 1){
+			var date2 = (response.list[i - 1].dt_txt);
+			var check = false;
+			var d1 = date.substring(0,10);
+			var d2 = date2.substring(0,10);
+			if( d1 === d2){
+				check = true;
+			}else{
+				check = false;
+			}
 
-	
-	
-	$("#displayData").append('<table id="city'+i+'" style="background-color:' +backgroundColor+ ';" width="320" border="1"><tr><td colspan="2">Date ' + date + '</td></tr><tr><td>City</td><td colspan="2" rowspan="1">' + city + '</td></tr><tr><td>Humidity</td><td width="118" class="humid">' + humidity + '%</td></tr><tr><td>Wind Speed</td><td width="186" class="wind">' + windSpeed + ' km/h</td></tr><tr><td>Weather</td><td width="186">' + description + '</td></tr><tr><td>Temperature</td><td width="186" class="temp">' + temperature + '&deg;C</td></tr></table>');
+			if(check == true){
+				backgroundColor = information.greenBackground;
+			} else {
+				backgroundColor = information.blueBackground;
+			}
+		}*/
+		
 
-
+		$("#displayData").append('<table id="city'+i+'" style="background-color:' +backgroundColor+ ';" width="320" border="1"><tr><td colspan="2">Date ' + date + '</td></tr><tr><td>City</td><td colspan="2" rowspan="1">' + city + '</td></tr><tr><td>Humidity</td><td width="118" class="humid">' + humidity + '%</td></tr><tr><td>Wind Speed</td><td width="186" class="wind">' + windSpeed + ' km/h</td></tr><tr><td>Weather</td><td width="186">' + description + '</td></tr><tr><td>Temperature</td><td width="186" class="temp">' + temperature + '&deg;C</td></tr></table>');
 	}
 	$("#hiddenCity").text(response.city.name);
 	
@@ -56,7 +97,7 @@ function compare(){
 	var averageTemp = 0;
 	var finalNumber;
 	var finalTempNumber;
-	var message = "";
+	var allMessages = {};
 	for(var i = 0; i < humidity.length; i++){
 		var stringHumid = humidity[i].innerHTML;
 		var stringTemp = temp[i].innerHTML;
@@ -70,95 +111,75 @@ function compare(){
 		}
 	}
 
-	if(finalNumber >= 40 && finalNumber <= 50){
-		message = "This is good weather and you should visit this city.";
-	} else {
-		message = "Bad weather, should not go here.";
-	}
+	allMessages = checkInfor(finalNumber);
 
-	$("#displayAverage").append('<p>Prediction for '+ displayCity +'</p><table style="background-color:red;" width="320" border="1"><tr><td>Average Humidity</td><td>' + finalNumber + '</td></tr><tr><td>Average Temperature</td><td>' + finalTempNumber + '</td></tr><tr><td>Recommendation</td><td>' + message+'</td></tr></table>');
+
+	$("#displayAverage").append('<p>Prediction for '+ displayCity +'</p><table style="background-color:'+allMessages.color+';" width="320" border="1"><tr><td>Average Humidity</td><td>' + finalNumber + '</td></tr><tr><td>Average Temperature</td><td>' + finalTempNumber + '</td></tr><tr><td>Recommendation</td><td>' + allMessages.message+'</td></tr></table>');
 	
 
 }
 
-
-function clear(){
-	$("#displayData").html('');
-	$("#displayAverage").html('');
-}
-
-
 var app = angular.module('myApp', []);
 app.controller('countriesCtrl', function($scope, $http) {
- 		$http.get("http://api.population.io/1.0/countries")
+	//@webservice
+ 		$http.get(information.url2)
     	.then(function (response) {
     		var list = response.data.countries;
     		$scope.countries = list;
     	});
-   
    	$scope.populationSearch = function() {
 		var country = document.getElementById("selectedCountry").value;
+		//var country = document.getElementById("selectedCountry");
 		var age = document.getElementById("age");
 		var year = document.getElementById("year");
 		var today = document.getElementById("today");
-		var anydate = document.getElementById("anydate");
-		var urlPopulation;
-		
-		if(country != "" && year.value != "" && age.value != ""){
-			urlPopulation = "http://api.population.io:80/1.0/population/" +year.value+ "/" + country.substring(7)+"/" + age.value + "/";
-			$.ajax({
-				dataType: 'json',
-				url: urlPopulation,
-				success: function (response){
-					//getPopulationForSpecificAge(response);
-					$scope.specificdata = response;
-				},
-			});
-		}
-		else if(country != "" && year.value != ""){
-			urlPopulation = "http://api.population.io:80/1.0/population/" +year.value+ "/" + country.substring(7)+"/";
-			$.ajax({
-				dataType: 'json',
-				url: urlPopulation,
-				success: function (response){
-					//response.forEach(poplateData);
-					$scope.alldata = response;
-				},
-			});
-		} 
+		var anydate = document.getElementById("datepicker");
+		var checkStatus = false;
+		$.var = information.url3;
 
+		if(year.value != ""){
+		 	$.var += year.value+"/";
+		 }
+
+		 if(country.substring(7) != ""){
+		 	$.var += country.substring(7)+"/";
+		 	$scope.country = country.substring(7);
+		 }
+
+		 if(age.value != ""){
+		 	$.var += age.value+"/";
+		 }
+
+		if(anydate.value != ""){
+		 	$.var += anydate.value+"/";
+		 }
+		 
+		//var paramData = {"/":year.value, "/":country.substring(7),"/":age.value, "/":anydate.value}
+		//urlPopulation = $.var;
+		//@webservice
+		$.ajax({
+			documentataType: 'json',
+			url: $.var,
+			data: {},
+			success: function (response){
+				if(anydate.value != ""){
+					$scope.specificdate = response;
+				}else{
+					$scope.alldata = response;
+				}
+			},
+		});
    	};
 });
 
-
-
-function getPopulationForSpecificAge(data){
-	var table = $("<table/>").addClass('tbPopulation');
-   	var tr = $("<tr/>");
-
+function comparePop(males, females){
 
 }
 
-function poplateData(element, index, response){
-	var total = 0;
-	total += +(element.females + element.males)
-	element.age;
-	element.year;
-	element.total;
-	element.country;
-	var table = $("<table/>").addClass('tbPopulation');
-   	var tr = $("<tr/>");
-   	var td = $("<td/>");
-   	var td1 = $("<td/>");
-   	var td2 = $("<td/>");
-   	var td3 = $("<td/>");
-   	td.append("Total");
-   	td1.append("Age");
-   	td2.append(element.total);
-   	td3.append(element.age);
-   	tr.append(td1,td3,td,td2);
-   	table.append(tr);
-	$("#totalPopulation").append(table);
+function checkInfor(number){
+	if(40 <= number && number <= 50){
+		return {message:information.good, color:'lightgreen'};
+	} else {
+		return {message:information.bad, color:'red'};
+	}
 }
-	
-
