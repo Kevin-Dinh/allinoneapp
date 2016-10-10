c94c21230f07b1b54b626b2b7fb45820cd9f1579
Kevin-Dinh <kevin.dinh90@gmail.com>
2016-10-10 12:30:56 +0200

    Add files via upload

---
+++ expressServer.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+//Pass the folder's name so files can be use directly
+app.use(express.static('public'));
+
+app.get('/allinoneapp.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "allinoneapp.html" );
+});
+
+var server = app.listen(8080, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+   console.log("Allinoneapp is listening at http://%s:%s", host, port)
+
+});
