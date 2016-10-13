ba117166678938b0577cba19420b0db0ba0446d3
Dinh Viet Duc <ddinhviet@dinhvietubuntb.ux.gratex.com>
2016-10-13 13:10:19 +0200

    update-code-review

--- public/generic.js
+++ public/generic.js
@@ -1,163 +1,142 @@
-var app = angular.module('myApp', []);
-app.controller('countriesCtrl', function($scope, $http) {
+$( "#searchWeather" ).bind( "click", function() {
+	$id = $("#locationId").val();
+	$param = {id:$id, APPID: $information.applicationID};
+
+	getWeather($information.url1, $param, function(weatherData){
+		createDataTable(weatherData);
+	}, function(errorHandler){
+		typeof errorHandler.responseJSON != "undefined" ? $("#errorHandlerForWeatherTab").text(errorHandler.responseJSON.detail) : $("#errorHandlerForWeatherTab").text("Invalid input!");
+		
+	})
+});
+
+$app = angular.module('myApp', []);
+$app.controller('countriesCtrl', function($scope, $http) {
 	//@webservice
- 		$http.get(information.url2)
+ 		$http.get($information.url2)
     	.then(function (countries) {
-    		var list = countries.data.countries;
-    		$scope.countries = list;
+    		$list = countries.data.countries;
+    		$scope.countries = $list;
     	});
+
    	$scope.populationSearch = function() {
-		var country = document.getElementById("selectedCountry").value;
-		var age = document.getElementById("age");
-		var year = document.getElementById("year");
-		var today = document.getElementById("today");
-		var anydate = document.getElementById("datepicker");
-		var checkStatus = false;
-		var url = information.url3;
+		$checkStatus = false;
+		$url = $information.url3;
 
-		if(year.value != ""){
-		 	url += year.value.trim()+"/";
+		$year = $("#year").val();
+        if($year != "") {
+             $url += encodeURIComponent($year.trim()) + "/";
+        }
+
+        $country = $("#selectedCountry").val().substring(7);
+		 if($country != ""){
+		 	 $url += $country+"/";
+		 	$scope.country = $country;
 		 }
 
-		 if(country.substring(7) != ""){
-		 	url += encodeURIComponent(country.substring(7))+"/";
-		 	$scope.country = country.substring(7);
+		 $age = $("#age").val();
+		 if($age != ""){
+		 	 $url += $age.trim()+"/";
 		 }
 
-		 if(age.value != ""){
-		 	url += age.value.trim()+"/";
+		 $specificDate = $("#datepicker").val();
+		if($specificDate != ""){
+		 	 $url += encodeURIComponent($specificDate) +"/";
 		 }
 
-		if(anydate.value != ""){
-		 	url += anydate.value.trim()+"/";
-		 }
 
-		//@webservice
-		$.ajax({
-			dataType: 'json',
-			url: url,
-			data: {},
-			success: function (population){
-				if(anydate.value != ""){
+
+		 ;
+		 getPopulationData($url, function (population) {
+		 	if($specificDate != ""){
 					$scope.specificdate = population;
 				}else{
 					$scope.alldata = population;
 					$scope.header = [1];
 				}
-			},
-		});
+		 }, function (errorHandler){
+		
+		 	typeof errorHandler.responseJSON != "undefined" ? $("#errorHandler").text(errorHandler.responseJSON.detail) : $("#errorHandler").text("Invalid input!");
+		 });
+		
    	};
 
    	$scope.emptyAllTxtBox = function(){
-		$("#selectedCountry").val('');
-		$("#age").val('');
-		$("#year").val('');
-		$("#datepicker").val('');
-		$("#today").val('');
+		$("#selectedCountry, #age, #year, #datepicker, #today").val('');
    	};
 
    	$scope.showBiggerNumber = function(){
-		var male = document.getElementsByClassName("male");
-		var female = document.getElementsByClassName("female");
-		var maleTotal;
-		var femaleTotal;
-		var checkBiggerNumber = false;
-		var color;
-		for(var i = 0; i <= male.length - 1; i++){
-			if(male[i] != 'undefined'){
-				maleTotal = male[i].innerHTML;
-				femaleTotal = female[i].innerHTML;
-				checkBiggerNumber = compare(maleTotal, femaleTotal);
-				color = assignColor(checkBiggerNumber);
-				if(color == 'red'){
-					$scope.checkColor = checkBiggerNumber;
-				}else{
-					$scope.checkColor;
-				}
-			}
+		for ($i = $(".male").length - 1; $i >= 0; $i--) {
+			$a = $(".male").val();
+			$.checkBiggerNumber = compare($(".male")[i].val(), $(".female")[$i].val());
+			$.color = assignColor($.checkBiggerNumber);
 			
-			//$scope.colorClass = color;
-			//var text = angular.element(document.querySelector('.male'));
-			//$scope.colorClass.addClass(color);
+			$color == 'red' ? $scope.checkColor = $.checkBiggerNumber  : $scope.checkColor;
 		}
-
-		
 	};
 });
 
-$( function() {
+$(function() {
     $( "#datepicker" ).datepicker({
     	dateFormat: 'yy-mm-dd',
     });
+
   });
 
 function compare(malePopulation, femalePopulation){
-	if(malePopulation > femalePopulation){
-		return true;
-	}
+	return malePopulation > femalePopulation ? true : "";
 }
 
 function assignColor(data){
-	if(data == true){
-		return colorObj[5];
-	}
+	return data == true ? $colorObj[5] : "";
 }
 
 function refreshPage(){
 	location.reload();
 }
 
-function searchForWeather(){
-	var id = document.getElementById("locationId");
-	var locationId = id.value;
-	var param = {id:locationId, APPID: information.APPID}
-	//@webservice
-	$.ajax({
-		dataType: 'jsonp',
-		url: information.url1,
-		data: param,
-		success: function (weatherData){
-			createDataTable(weatherData);
-		},
-	});
-	
-};
+
 
 function createDataTable(weatherList){
-	var counter = 0;
-	document.getElementById("city").innerHTML = "Weather forecast from " + weatherList.list[0].dt_txt + " to " + weatherList.list[weatherList.list.length -1].dt_txt; 
-	for (var i = 0; i < weatherList.list.length; i++) { 
-    	var currentDateWithTime = weatherList.list[i].dt_txt;
-    	var currentDate = currentDateWithTime.substring(0,10);
-		var city = weatherList.city.name;
-		var humidity = weatherList.list[i].main.humidity;
-		var windSpeed = weatherList.list[i].wind.speed;
-		var description = weatherList.list[i].weather[0].description;
-		var temperature = Math.round((weatherList.list[i].main.temp - 273.15))*100/100;
+	$counter = 0;
+	$("#city").text("Weather forecast from " + weatherList.list[0].dt_txt + " to " + weatherList.list[weatherList.list.length -1].dt_txt); 
 
-		var backgroundColor;
 
+	$listOfWeather = weatherList.list.length;
+	for ($i = 0; $i < $listOfWeather; $i++) { 
+    	$currentDateWithTime = weatherList.list[$i].dt_txt;
+    	$currentDate = $currentDateWithTime.substring(0,10);
 		
 		/*
 			Compare current date with 
 			privious date to apply 
 			background color
 		*/
-		if(i == 0){
-			backgroundColor = colorObj[counter];
+		if($i == 0){
+			$backgroundColor = $colorObj[$counter];
 		} else {
-			var previousDateWithTime = weatherList.list[i - 1].dt_txt;
-			var previousDate = previousDateWithTime.substring(0,10);
+			$previousDateWithTime = weatherList.list[$i - 1].dt_txt;
+			$previousDate = $previousDateWithTime.substring(0,10);
 
-			if(currentDate === previousDate){
-				backgroundColor = colorObj[counter];
-			} else {
-				counter++;
-				backgroundColor = colorObj[counter];
-			}
+			$currentDate === $previousDate ? $backgroundColor = $colorObj[$counter] : $backgroundColor = $colorObj[++$counter];
 		}
+
+		$mainPart = weatherList.list[$i];
+		$city = weatherList.city.name;
+		$humidity = $mainPart.main.humidity;
+		$windSpeed = $mainPart.wind.speed;
+		$description = $mainPart.weather[0].description;
+		$temperature = Math.round(($mainPart.main.temp - 273.15))*100/100;
 		
-		$("#displayData").append('<table id="city'+i+'" style="background-color:' +backgroundColor+ ';" width="320" border="1"><tr><td colspan="2">Date ' + currentDateWithTime + '</td></tr><tr><td>City</td><td colspan="2" rowspan="1">' + city + '</td></tr><tr><td>Humidity</td><td width="118" class="humid">' + humidity + '%</td></tr><tr><td>Wind Speed</td><td width="186" class="wind">' + windSpeed + ' km/h</td></tr><tr><td>Weather</td><td width="186">' + description + '</td></tr><tr><td>Temperature</td><td width="186" class="temp">' + temperature + '&deg;C</td></tr></table>');
+		$("#displayData").append('<table id="city'+
+			$i+'" style="background-color:' +
+			$backgroundColor+ ';" width="320" border="1"><tr><td colspan="2">Date ' + 
+			$currentDateWithTime + '</td></tr><tr><td>City</td><td colspan="2" rowspan="1">' + 
+			$city + '</td></tr><tr><td>Humidity</td><td width="118" class="humid">' + 
+			$humidity + '%</td></tr><tr><td>Wind Speed</td><td width="186" class="wind">' + 
+			$windSpeed + ' km/h</td></tr><tr><td>Weather</td><td width="186">' + 
+			$description + '</td></tr><tr><td>Temperature</td><td width="186" class="temp">' + 
+			$temperature + '&deg;C</td></tr></table>');
 
 	}
 	//Add city to use later in UI
@@ -169,56 +148,64 @@ function createDataTable(weatherList){
 	This function will display average data
 */
 function getAverageWeather(){
-	var humidity = document.getElementsByClassName("humid");
-	var temp = document.getElementsByClassName("temp");
-	var averageWind = document.getElementsByClassName("wind");
-	var city = document.getElementById("hiddenCity");
-	var displayCity = city.innerHTML;
-	var totalHumidity = 0;
-	var totalTemp = 0;
-	var finalNumber;
-	var finalTempNumber;
-	var allMessages = {};
-	for(var i = 0; i < humidity.length; i++){
-		var stringHumid = humidity[i].innerHTML;
-		var stringTemp = temp[i].innerHTML;
-		var tempOnlyNumber = stringTemp.substring(0, stringTemp.length - 2);
-		var onlyNumber = stringHumid.substring(0, stringHumid.length - 1);
-		totalTemp += +tempOnlyNumber;
-		totalHumidity += +onlyNumber;
-	}
 
-	finalNumber = roundNumberToTwoDecimal(totalHumidity, humidity.length - 1);
+	$humidity = $(".humid");
+	$temp = $(".temp"),
+	$city = $("#hiddenCity");
 
-	finalTempNumber = roundNumberToTwoDecimal(totalTemp, humidity.length - 1);
+	$humidityList = $.map($humidity, function($value, $index){
+        return $value.innerHTML.replace("%","");
+    });
 
-	allMessages = checkInfor(finalNumber);
+	$totalH = 0;
+	$totalHumidity = $.each($humidityList, function(index, value){
 
+		return $totalH += +value;
+	});
 
-	$("#displayAverage").append('<p>Prediction for '+ displayCity +'</p><table style="background-color:'+allMessages.color+';" width="320" border="1"><tr><td>Average Humidity</td><td>' + finalNumber + '</td></tr><tr><td>Average Temperature</td><td>' + finalTempNumber + '</td></tr><tr><td>Recommendation</td><td>' + allMessages.message+'</td></tr></table>');
+    $temperatureList = $.map($temp, function(value, index){
+        return value.innerHTML.replace("°C","");
+    });
+
+    $totalTemp = 0;
+    $totalHumidity = $.each($temperatureList, function(index, value){
+		return $totalTemp += +value;
+	});
+
+	$finalHumidity = roundNumberToTwoDecimal($totalH, $humidity.length - 1);
+	$finalTempNumber = roundNumberToTwoDecimal($totalTemp, $temp.length - 1);
+	$allMessages = checkInfor($finalHumidity);
+	$displayCity = $city.text();
+
+	$("#displayAverage").append('<p>Prediction for '+ 
+		$displayCity +'</p><table style="background-color:'+
+		$allMessages.color+';" width="320" border="1"><tr><td>Average Humidity</td><td>' + 
+		$finalHumidity + '</td></tr><tr><td>Average Temperature</td><td>' + 
+		$finalTempNumber + '</td></tr><tr><td>Recommendation</td><td>' + 
+		$allMessages.message+'</td></tr></table>');
 }
-
 
 function roundNumberToTwoDecimal(total, length){
-	var average = Math.round(total/length)*100/100;
-	return average;
+	return $average = Math.round(total/length)*100/100;
 }
+
 
 function checkInfor(number){
-	if(40 <= number && number <= 50){
-		return {message:information.good, color:colorObj[0]};
-	} else {
-		return {message:information.bad, color:colorObj[5]};
-	}
+	return (40 <= number && number <= 50) ? {message:$information.goodWeather, color:$colorObj[0]} : {message:$information.badWeather, color:$colorObj[5]};
 }
 
-var information = {
-	good:"This is good weather and you should visit this city.",
-	bad:"Bad weather, should not go here.",
-	url1:"http://api.openweathermap.org/data/2.5/forecast?",
+$information = {
+	goodWeather:"This is good weather and you should visit this city.",
+	badWeather:"Bad weather, should not go here.",
+	applicationID:"4066916114404bb6ee654b8bbc254890"
+}
+
+$web_services = {
+	url1:"http://api.openweathermap.org/data/2.5/forecast",
 	url2:"http://api.population.io/1.0/countries",
 	url3:"http://api.population.io:80/1.0/population/",
-	APPID:"4066916114404bb6ee654b8bbc254890"
 }
 
-var colorObj = ['lightgreen', 'lightblue', 'silver', 'brown', 'white', 'red'];
+Object.assign($information, $web_services);
+
+$colorObj = ['lightgreen', 'lightblue', 'silver', 'brown', 'white', 'red'];
